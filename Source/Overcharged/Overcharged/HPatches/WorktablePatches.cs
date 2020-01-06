4657baa1b5d3bc7e8853bea04bfe60c16a194775
FxWolfxe <ndmuench@comcast.net>
2020-01-05 19:18:25 -0500

    made new infuser thing class that will use bills to infuse things

---
+++ Source/Overcharged/Overcharged/HPatches/WorktablePatches.cs
@@ -0,0 +1,23 @@
+﻿// WorktablePatches.cs created by Iron Wolf for Overcharged on 01/05/2020 7:11 PM
+// last updated 01/05/2020  7:11 PM
+
+using Harmony;
+using Verse;
+
+namespace Overcharged.HPatches
+{
+    static class WorktablePatches
+    {
+        [HarmonyPatch(typeof(InfusionChamber)), HarmonyPatch(nameof(InfusionChamber.CurrentlyUsableForBills))]
+        static class MakeIsUsableVirtual //this is needed because CurrentlyUsableForBills isn't virtual and the bills tab is hardcoded to use the work table building :V 
+        {
+
+            static bool Prefix(InfusionChamber __instance, ref bool __result)
+            {
+                Log.Message($"patching for {__instance.Label}");
+                __result = __instance.CurrentlyUsableForBillsReal();
+                return false; 
+            }
+        }
+    }
+}
---
+++ Source/Overcharged/Overcharged/InfusionChamber.cs
@@ -0,0 +1,81 @@
+﻿// InfusionChamber.cs created by Iron Wolf for Overcharged on 01/05/2020 6:43 PM
+// last updated 01/05/2020  6:43 PM
+
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Overcharged
+{
+    public class InfusionChamber : Building_WorkTable, ILightningReceiverThing,  IThingHolder
+    {
+        private CompAffectedByFacilities _comp;
+
+        
+        private ThingOwner _container;
+
+        public InfusionChamber()
+        {
+            _container = new ThingOwner<Thing>(this);
+        }
+
+        public bool CurrentlyUsableForBillsReal()
+        {
+            return _container.Count == 0; 
+        }
+
+
+        /// <summary>Strikes this instance with the specified energy.</summary>
+        /// <param name="energy">The energy.</param>
+        public virtual void Strike(int energy)
+        {
+            IEnumerable<ThingComp> comps = AllComps ?? Enumerable.Empty<ThingComp>();
+            foreach (ILightningReceiver lightningReceiver in comps.OfType<ILightningReceiver>()) lightningReceiver.Strike(energy);
+        }
+
+        public void GetChildHolders(List<IThingHolder> outChildren)
+        {
+            ThingOwnerUtility.AppendThingHoldersFromThings(outChildren, GetDirectlyHeldThings());
+        }
+
+        public ThingOwner GetDirectlyHeldThings()
+        {
+            return _container;
+        }
+
+        private CompAffectedByFacilities Comp => _comp ?? (_comp = GetComp<CompAffectedByFacilities>());
+
+        public override void DeSpawn(DestroyMode mode = DestroyMode.Vanish)
+        {
+            base.DeSpawn(mode);
+            IEnumerable<Thing> facilities = Comp?.LinkedFacilitiesListForReading ?? Enumerable.Empty<Thing>();
+            foreach (LightningRodBase lightningRodBase in facilities.OfType<LightningRodBase>())
+                if (lightningRodBase.IsLinkedTo(this))
+                    lightningRodBase.Notify_ReceiverRemoved(this);
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Deep.Look(ref _container, "container", this);
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+
+            if (this.IsHashIntervalTick(45))
+            {
+                IEnumerable<Thing> facilities = Comp?.LinkedFacilitiesListForReading ?? Enumerable.Empty<Thing>();
+                foreach (LightningRodBase lightningRodBase in facilities.OfType<LightningRodBase>())
+                {
+                    if (lightningRodBase.IsLinkedTo(this)) continue;
+
+                    lightningRodBase.Notify_ReceiverLinked(this);
+                }
+            }
+        }
+    }
+}
